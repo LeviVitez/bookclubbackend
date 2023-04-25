eabffe1a5bb6da4027cf77172de7a6edd32c9b36
LeviVitez <bmwyolo555@gmail.com>
2023-04-25 11:47:19 +0200

    payment entity

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import Payments from './Payments.entity';
 
 @Controller()
 export class AppController {
@@ -9,9 +10,25 @@ export class AppController {
     private dataSource: DataSource,
   ) {}
 
-  @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Post('/seed')
+  async fillpayments() {
+    const paymentrepo = this.dataSource.getRepository(Payments);
+    for (let i=0; i < 16; i++){
+      const payment=new Payments();
+      payment.id=i;
+      payment.amount= i = 1234;
+      payment.member_id= i;
+      payment.paid_at= "2023-04-25";
+      await paymentrepo.save(payment);
+    }
+    return Payments;
+  }
+
+  @Get(/api/beemmrs)
+  async getAllMembers() {
+    const membersrepo = this.dataSource.getRepository(Members);
+    return membersrepo.find();
+
+  @Body()
   }
 }
--- src/payments.entity.ts
+++ src/payments.entity.ts
@@ -1,1 +1,23 @@
-import {}
+import { IsString, Length, Min, IsNumber } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export default class Payments {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsString()
+  nev: string;
+
+  @Column()
+  @Min(5)
+  @IsNumber()
+  amount: number;
+
+  @Column()
+  member_id: number;
+
+  @Column()
+  paid_at: string;
+}
